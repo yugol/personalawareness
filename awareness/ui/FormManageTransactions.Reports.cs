578e95a18620070c51707a05926ee9de50dfb4fb
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2008-11-06 21:25:55 +0000

    added licence comments

--- awareness/ui/FormManageTransactions.Reports.cs
+++ awareness/ui/FormManageTransactions.Reports.cs
@@ -1,9 +1,31 @@
 /*
+ * Copyright (c) 2008 Iulian GORIAC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
  * Created by SharpDevelop.
  * User: Iulian
  * Date: 16/09/2008
  * Time: 22:02
- * 
+ *
  */
 using System;
 using System.Collections.Generic;
@@ -17,42 +39,37 @@ using awareness.db;
 
 namespace awareness.ui
 {
-    public partial class FormManageTransactions
-    {
-        void Button1Click(object sender, EventArgs e)
-        {
+    public partial class FormManageTransactions {
+        void Button1Click(object sender, EventArgs e){
             reportsMenu.Show(Cursor.Position);
         }
-        
-        void PieChartToolStripMenuItemClick(object sender, EventArgs e)
-        {
-            var expensesPie = from t in transactions 
-                where t.To.IsBudget && !((DalBudgetCategory) t.To).IsIncome
-                group t by t.To into slice
-                // orderby slice.Key.Name
-                select new {Name = slice.Key.Name, Ammount = slice.Sum(t => t.Ammount)};
-           
+
+        void PieChartToolStripMenuItemClick(object sender, EventArgs e){
+            var expensesPie = from t in transactions
+                              where t.To.IsBudget&&!((DalBudgetCategory) t.To).IsIncome
+                              group t by t.To into slice
+                              // orderby slice.Key.Name
+                              select new {Name = slice.Key.Name, Ammount = slice.Sum(t => t.Ammount)};
+
             int pieCount = expensesPie.Count();
             double[] values = new double[pieCount];
             string[] labels = new string[pieCount];
             int i = 0;
             decimal total = 0;
-            foreach (var slice in expensesPie)
-            {
+            foreach (var slice in expensesPie){
                 total += slice.Ammount;
                 values[i] = (double) slice.Ammount;
                 labels[i] = slice.Name;
                 ++i;
             }
 
-            for (i = 0; i < pieCount; ++i)
-            {
-                double perCent = values[i] * 100 / ((double) total);
+            for (i = 0; i < pieCount; ++i){
+                double perCent = values[i] *100 / ((double) total);
                 labels[i] = string.Format("{0}\n{1} ({2}%)", labels[i], UiUtil.FormatCurrency((decimal) values[i]), perCent.ToString("#0.00"));
             }
-                        
+
             FormReport report = new FormReport();
-            report.Text = "Expenses by Bugetary Caregories"; 
+            report.Text = "Expenses by Bugetary Caregories";
             GraphPane myPane = report.Graph.GraphPane;
             myPane.Fill = new Fill(Configuration.ALTERNATE_BACKGROUND, Configuration.ALTERNATE_BACKGROUND, 90f);
             myPane.Chart.Fill = new Fill(Configuration.NORMAL_BACKGROUND, Configuration.ALTERNATE_BACKGROUND, 90f);
@@ -60,9 +77,8 @@ namespace awareness.ui
             myPane.Legend.IsVisible = false;
             myPane.AddPieSlices(values, labels);
             report.Graph.AxisChange();
-            
+
             report.Show();
         }
-        
     }
 }
--- awareness/ui/UiUtil.cs
+++ awareness/ui/UiUtil.cs
@@ -1,9 +1,31 @@
 /*
+ * Copyright (c) 2008 Iulian GORIAC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
  * Created by SharpDevelop.
  * User: Iulian
  * Date: 21/09/2008
  * Time: 23:11
- * 
+ *
  */
 using System;
 using System.Collections.Generic;
@@ -14,33 +36,29 @@ using awareness.db;
 
 namespace awareness.ui
 {
-    internal class UiUtil
-    {
+    internal class UiUtil {
         // TODO: validarea se face doar la apasarea butonului si nu la pierdereaa focus-ului
-        
-        internal static string FormatCurrency(decimal ammount)
-        {
+
+        internal static string FormatCurrency(decimal ammount){
             return ammount.ToString("#,###,##0.00 RON");
         }
 
-        internal static void FillFoodConsumptionReasons(ComboBox whyCombo, DalReason what)
-        {
+        internal static void FillFoodConsumptionReasons(ComboBox whyCombo, DalReason what){
             whyCombo.Items.Clear();
 
             AwarenessDataContext dc = DbUtil.GetDataContext();
             IEnumerable<DalConsumer> consumers = from r in dc.transactionReasons.OfType<DalConsumer>()
-                orderby r.Name
-                select r;
+                                                 orderby r.Name
+                                                 select r;
             whyCombo.Items.Add("---Consumers---");
             foreach (DalConsumer consumer in consumers) {
                 whyCombo.Items.Add(consumer);
             }
-            
-            if (!(what is DalRecipe))
-            {
+
+            if (!(what is DalRecipe)){
                 IEnumerable<DalRecipe> recipes = from r in dc.transactionReasons.OfType<DalRecipe>()
-                    orderby r.Name
-                    select r;
+                                                 orderby r.Name
+                                                 select r;
                 whyCombo.Items.Add("");
                 whyCombo.Items.Add("---Recipes---");
                 foreach (DalRecipe recipe in recipes) {
@@ -48,6 +66,5 @@ namespace awareness.ui
                 }
             }
         }
-        
     }
 }
