6145244839bfe676a9afb72554dbb70015292610
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2008-12-11 13:23:31 +0000

    when uncheck action plan time remove reminders

--- awareness/ui/FormAbout.cs
+++ awareness/ui/FormAbout.cs
@@ -40,14 +40,7 @@ namespace Awareness.UI
     {
         public FormAbout()
         {
-            //
-            // The InitializeComponent() call is required for Windows Forms designer support.
-            //
             InitializeComponent();
-            
-            //
-            // TODO: Add constructor code after the InitializeComponent() call.
-            //
         }
     }
 }
