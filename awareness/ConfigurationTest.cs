6a3f791e07397de7812420217e13288ff552e3cd
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2009-02-15 15:45:08 +0000

    tray icon is always visible config file - holds last opened database and reopens it automatically

--- awareness/ConfigurationTest.cs
+++ awareness/ConfigurationTest.cs
@@ -1,16 +1,22 @@
 /*
- * Copyright (c) 2008 Iulian GORIAC
+ * Created by SharpDevelop.
+ * User: Iulian
+ * Date: 2/15/2009
+ * Time: 4:56 PM
  * 
+ *
+ * Copyright (c) 2008 Iulian GORIAC
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,27 +25,22 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+#if TEST
 
-/*
- * Created by SharpDevelop.
- * User: Iulian
- * Date: 30/08/2008
- * Time: 22:07
- *
- */
 using System;
-using System.Windows.Forms;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
 
-using Awareness.UI;
-
-namespace Awareness {
-    
-    internal sealed class Program {
-        [STAThread]
-        private static void Main(string[] args) {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
-        }
-    }
+namespace Awareness
+{
+	[TestFixture]
+	public class ConfigurationTest
+	{
+		[Test]
+		public void ConfigFileName()
+		{
+			Assert.AreEqual("C:\\Documents and Settings\\Iulian\\Application Data\\PersonalAwareness\\config.properties", Configuration.ConfigFileName);
+		}
+	}
 }
+#endif
