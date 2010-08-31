023fe1a1b638c52c838b87e2f0d24ed53977504f
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2010-08-31 10:18:01 +0000

    no more printf

--- branches/myawareness/adb-test/Date_test.cpp
+++ branches/myawareness/adb-test/Date_test.cpp
@@ -5,14 +5,12 @@ using namespace adb;
 
 TEST( Full, Date)
 {
-	char rep[20];
 	Date date;
 
 	date.setNow();
-	date.sprintf(rep);
+    // cout << date << endl;
 
 	date.setValue("20061107");
-	date.sprintf(rep);
 	LONGS_EQUAL(2006, date.getYear());
 	LONGS_EQUAL(11, date.getMonth());
 	LONGS_EQUAL(7, date.getDay());
