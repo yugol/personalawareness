9f6c91bad0f37bea9c1045a29c05ebc7c3d6ed44
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2010-09-01 08:51:03 +0000

    undo/redo UI support

--- branches/myawareness/adb/inc/DbUtil.h
+++ branches/myawareness/adb/inc/DbUtil.h
@@ -7,7 +7,7 @@ namespace adb {
 
     class DbUtil {
     public:
-        static const int STATEMENT_LEN = 2000;
+        static const int STATEMENT_LEN = 10000;
 
         static void charPtrToString(std::string &str, const char *cptr);
         static void trimSpaces(std::string& str);
--- branches/myawareness/adb/src/Transaction.cpp
+++ branches/myawareness/adb/src/Transaction.cpp
@@ -58,7 +58,7 @@ namespace adb {
             THROW(Exception::WRONG_VALUE_MESSAGE);
         }
 
-        // TBD: check if source != destination
+        // TBD+: check if source != destination
     }
 
 } // namespace adb
