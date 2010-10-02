17db2b0ac80e8597145358b864da4a3069c140ba
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2010-10-02 23:19:37 +0000

    SQL export bugfix show/hide zero balance account support

--- trunk/pcfdb/inc/Configuration.h
+++ trunk/pcfdb/inc/Configuration.h
@@ -5,116 +5,126 @@
 
 class Configuration {
 public:
-    // utilities
-    static const int LINE_BUFFER_LENGTH = 10000;
-    static const int DEFAULT_ID = 0;
-    static const char CONFIGURATION_FILEEXT[];
-    // defaults
-    static const char PROJECT_MARKER[]; // used for database identification
-    static const char PROJECT_NAME[];
-    static const char PROJECT_VERSION[];
-    static const char PROJECT_DATABASE_VERSION[];
-    static const char DEFAULT_CURRENCY_SYMBOL[];
-    static const bool DEFAULT_PREFIX_CURRENCY;
-    static const bool DEFAULT_COMPACT_TRNSACTIONS;
-    static const bool DEFAULT_COMPARE_ASCII_ONLY;
-    // tables
-    static const char TABLE_PREFERENCES[];
-    static const char TABLE_ACCOUNTS[];
-    static const char TABLE_ITEMS[];
-    static const char TABLE_TRANSACTIONS[];
-    // indexes
-    static const char INDEX_MARKER[];
-    // columns
-    static const char COLUMN_ID[];
-    static const char COLUMN_DELETED[];
-    static const char COLUMN_TYPE[];
-    static const char COLUMN_NAME[];
-    static const char COLUMN_GROUP[];
-    static const char COLUMN_BALANCE[];
-    static const char COLUMN_COMMENT[];
-    static const char COLUMN_TRANSACTION[];
-    static const char COLUMN_DATE[];
-    static const char COLUMN_VALUE[];
-    static const char COLUMN_SOURCE[];
-    static const char COLUMN_DESTINATION[];
-    static const char COLUMN_ITEM[];
-    // preference names
-    static const char PREF_PROJECT_MARKER[];
-    static const char PREF_DATABASE_VERSION[];
-    static const char PREF_CURRENCY_SYMBOL[];
-    static const char PREF_PREFIX_CURRENCY[];
-    static const char PREF_COMPACT_TRNSACTIONS[];
-    static const char PREF_COMPARE_ASCII_ONLY[];
+	// utilities
+	static const int LINE_BUFFER_LENGTH = 10000;
+	static const int DEFAULT_ID = 0;
+	static const char CONFIGURATION_FILEEXT[];
+	// defaults
+	static const char PROJECT_MARKER[]; // used for database identification
+	static const char PROJECT_NAME[];
+	static const char PROJECT_VERSION[];
+	static const char PROJECT_DATABASE_VERSION[];
+	static const char DEFAULT_CURRENCY_SYMBOL[];
+	static const bool DEFAULT_PREFIX_CURRENCY;
+	static const bool DEFAULT_COMPACT_TRNSACTIONS;
+	static const bool DEFAULT_COMPARE_ASCII_ONLY;
+	static const bool DEFAULT_HIDE_ZERO_BALANCE_ACCOUNTS;
+	// tables
+	static const char TABLE_PREFERENCES[];
+	static const char TABLE_ACCOUNTS[];
+	static const char TABLE_ITEMS[];
+	static const char TABLE_TRANSACTIONS[];
+	// indexes
+	static const char INDEX_MARKER[];
+	// columns
+	static const char COLUMN_ID[];
+	static const char COLUMN_DELETED[];
+	static const char COLUMN_TYPE[];
+	static const char COLUMN_NAME[];
+	static const char COLUMN_GROUP[];
+	static const char COLUMN_BALANCE[];
+	static const char COLUMN_COMMENT[];
+	static const char COLUMN_TRANSACTION[];
+	static const char COLUMN_DATE[];
+	static const char COLUMN_VALUE[];
+	static const char COLUMN_SOURCE[];
+	static const char COLUMN_DESTINATION[];
+	static const char COLUMN_ITEM[];
+	// preference names
+	static const char PREF_PROJECT_MARKER[];
+	static const char PREF_DATABASE_VERSION[];
+	static const char PREF_CURRENCY_SYMBOL[];
+	static const char PREF_PREFIX_CURRENCY[];
+	static const char PREF_COMPACT_TRNSACTIONS[];
+	static const char PREF_COMPARE_ASCII_ONLY[];
+	static const char PREF_HIDE_ZERO_BALANCE_ACCOUNTS[];
 
-    static Configuration* instance();
+	static Configuration* instance();
 
-    virtual ~Configuration();
+	virtual ~Configuration();
 
-    bool existsConfigurationFile() const;
-    const std::string& getConfigurationFilePath() const;
-    const std::string& getLastDatabasePath() const;
-    const std::string& getCurrencySymbol() const;
-    bool isPrefixCurrency() const;
-    bool isCompactTransactions() const;
-    bool isCompareAsciiOnly() const;
+	bool existsConfigurationFile() const;
+	const std::string& getConfigurationFilePath() const;
+	const std::string& getLastDatabasePath() const;
+	const std::string& getCurrencySymbol() const;
+	bool isPrefixCurrency() const;
+	bool isCompactTransactions() const;
+	bool isCompareAsciiOnly() const;
+	bool isHideZeroBalanceAccounts() const;
 
-    void setLastDatabasePath(const char*);
-    void setCurrencySymbol(const char*);
-    void setPrefixCurrency(const char*);
-    void setCompactTransactions(const char*);
-    void setCompareAsciiOnly(const char*);
-    void setPrefixCurrency(bool);
-    void setCompactTransactions(bool);
-    void setCompareAsciiOnly(bool);
+	void setLastDatabasePath(const char*);
+	void setCurrencySymbol(const char*);
+	void setPrefixCurrency(const char*);
+	void setCompactTransactions(const char*);
+	void setCompareAsciiOnly(const char*);
+	void setPrefixCurrency(bool);
+	void setCompactTransactions(bool);
+	void setCompareAsciiOnly(bool);
+	void setHideZeroBalanceAccounts(bool);
 
 private:
-    static Configuration* instance_;
+	static Configuration* instance_;
 
-    std::string configurationFilePath_;
-    std::string lastDatabasePath_;
+	std::string configurationFilePath_;
+	std::string lastDatabasePath_;
 
-    std::string currencySymbol_;
-    bool prefixCurrency_;
-    bool compactTransactions_;
-    bool compareAsciiOnly_;
+	std::string currencySymbol_;
+	bool prefixCurrency_;
+	bool compactTransactions_;
+	bool compareAsciiOnly_;
+	bool hideZeroBalanceAccounts_;
 
-    Configuration();
-    Configuration(const Configuration&);
-    void operator=(const Configuration&);
+	Configuration();
+	Configuration(const Configuration&);
+	void operator=(const Configuration&);
 
-    void readConfiguration();
-    void writeConfiguration();
+	void readConfiguration();
+	void writeConfiguration();
 };
 
 inline const std::string& Configuration::getConfigurationFilePath() const
 {
-    return configurationFilePath_;
+	return configurationFilePath_;
 }
 
 inline const std::string& Configuration::getLastDatabasePath() const
 {
-    return lastDatabasePath_;
+	return lastDatabasePath_;
 }
 
 inline const std::string& Configuration::getCurrencySymbol() const
 {
-    return currencySymbol_;
+	return currencySymbol_;
 }
 
 inline bool Configuration::isPrefixCurrency() const
 {
-    return prefixCurrency_;
+	return prefixCurrency_;
 }
 
 inline bool Configuration::isCompactTransactions() const
 {
-    return compactTransactions_;
+	return compactTransactions_;
 }
 
 inline bool Configuration::isCompareAsciiOnly() const
 {
-    return compareAsciiOnly_;
+	return compareAsciiOnly_;
+}
+
+inline bool Configuration::isHideZeroBalanceAccounts() const
+{
+	return hideZeroBalanceAccounts_;
 }
 
 #endif /* CONFIGURATIO_H_ */
--- trunk/pcfdb/src/Configuration.cpp
+++ trunk/pcfdb/src/Configuration.cpp
@@ -1,3 +1,19 @@
+/*
+ * To add a new preference to the database:
+ * - export the old database as SQL
+ * - add the preference support in Configuration class
+ *   - add Configuration::PREF_ variable
+ *   - add Configuration::DEFAULT_ variable
+ *   - add Configuration::_ attribute
+ *   - add getter and setter member functions
+ * - update CreateDatabase::buildSqlCommand function to insert the new preference
+ * - update DatabaseConnection::readPreferences function to read the new preference from the database
+ * - update DatabaseConnection::writePreferences function to write the new preference to database
+ * - update Configuration::PROJECT_DATABASE_VERSION
+ * - create a new database
+ * - import database from SQL script
+ */
+
 #include <cstdlib>
 #include <sstream>
 #include <fstream>
@@ -15,12 +31,14 @@ const char Configuration::CONFIGURATION_FILEEXT[] = ".awareness.cfg";
 // defaults
 const char Configuration::PROJECT_MARKER[] = "5A08548C-B8C3-11DF-8AC9-BD12E0D72085-PERSONALAWARENESS";
 const char Configuration::PROJECT_NAME[] = "Personal Cash Flow";
-const char Configuration::PROJECT_VERSION[] = "0.7.2 alpha";
-const char Configuration::PROJECT_DATABASE_VERSION[] = "0.1";
+const char Configuration::PROJECT_VERSION[] = "0.7.3 alpha";
+const char Configuration::PROJECT_DATABASE_VERSION[] = "0.1.1";
 const char Configuration::DEFAULT_CURRENCY_SYMBOL[] = "'";
 const bool Configuration::DEFAULT_PREFIX_CURRENCY = false;
 const bool Configuration::DEFAULT_COMPACT_TRNSACTIONS = false;
 const bool Configuration::DEFAULT_COMPARE_ASCII_ONLY = false;
+const bool Configuration::DEFAULT_HIDE_ZERO_BALANCE_ACCOUNTS = true;
+
 // tables
 const char Configuration::TABLE_PREFERENCES[] = "preferences";
 const char Configuration::TABLE_ACCOUNTS[] = "accounts";
@@ -49,28 +67,30 @@ const char Configuration::PREF_CURRENCY_SYMBOL[] = "CURRENCY_SYMBOL";
 const char Configuration::PREF_PREFIX_CURRENCY[] = "PREFIX_CURRENCY";
 const char Configuration::PREF_COMPACT_TRNSACTIONS[] = "COMPACT_TRNSACTIONS";
 const char Configuration::PREF_COMPARE_ASCII_ONLY[] = "COMPARE_ASCII_ONLY";
+const char Configuration::PREF_HIDE_ZERO_BALANCE_ACCOUNTS[] = "PREF_HIDE_ZERO_BALANCE_ACCOUNTS";
 
 Configuration* Configuration::instance()
 {
-    if (0 == instance_) {
-        instance_ = new Configuration();
-    }
-    return instance_;
+	if (0 == instance_) {
+		instance_ = new Configuration();
+	}
+	return instance_;
 }
 
 Configuration::Configuration() :
-    currencySymbol_(DEFAULT_CURRENCY_SYMBOL), prefixCurrency_(DEFAULT_PREFIX_CURRENCY), compactTransactions_(DEFAULT_COMPACT_TRNSACTIONS), compareAsciiOnly_(DEFAULT_COMPARE_ASCII_ONLY)
+	currencySymbol_(DEFAULT_CURRENCY_SYMBOL), prefixCurrency_(DEFAULT_PREFIX_CURRENCY), compactTransactions_(DEFAULT_COMPACT_TRNSACTIONS),
+			compareAsciiOnly_(DEFAULT_COMPARE_ASCII_ONLY), hideZeroBalanceAccounts_(DEFAULT_HIDE_ZERO_BALANCE_ACCOUNTS)
 {
-    const char* homeFolder = ::getenv(HOME_ENVIRONMENT_VARIABLE_NAME);
-    // TBD+: show error message and start with no initial file
-    if (NULL == homeFolder) {
-        THROW("HOME environment variable not found");
-    } else {
-        ostringstream sout;
-        sout << homeFolder << "/" << CONFIGURATION_FILEEXT;
-        configurationFilePath_ = sout.rdbuf()->str();
-        readConfiguration();
-    }
+	const char* homeFolder = ::getenv(HOME_ENVIRONMENT_VARIABLE_NAME);
+	// TBD+: show error message and start with no initial file
+	if (NULL == homeFolder) {
+		THROW("HOME environment variable not found");
+	} else {
+		ostringstream sout;
+		sout << homeFolder << "/" << CONFIGURATION_FILEEXT;
+		configurationFilePath_ = sout.rdbuf()->str();
+		readConfiguration();
+	}
 }
 
 Configuration::~Configuration()
@@ -79,66 +99,71 @@ Configuration::~Configuration()
 
 bool Configuration::existsConfigurationFile() const
 {
-    ifstream fin(configurationFilePath_.c_str());
-    return fin.is_open();
+	ifstream fin(configurationFilePath_.c_str());
+	return fin.is_open();
 }
 
 void Configuration::setLastDatabasePath(const char* location)
 {
-    DbUtil::charPtrToString(lastDatabasePath_, location);
-    DbUtil::trimSpaces(lastDatabasePath_);
-    writeConfiguration();
+	DbUtil::charPtrToString(lastDatabasePath_, location);
+	DbUtil::trimSpaces(lastDatabasePath_);
+	writeConfiguration();
 }
 
 void Configuration::setCurrencySymbol(const char* symbol)
 {
-    DbUtil::charPtrToString(currencySymbol_, symbol);
-    DbUtil::trimSpaces(currencySymbol_);
+	DbUtil::charPtrToString(currencySymbol_, symbol);
+	DbUtil::trimSpaces(currencySymbol_);
 }
 
 void Configuration::setPrefixCurrency(const char* cstr)
 {
-    prefixCurrency_ = DbUtil::toBool(cstr);
+	prefixCurrency_ = DbUtil::toBool(cstr);
 }
 
 void Configuration::setCompactTransactions(const char* cstr)
 {
-    compactTransactions_ = DbUtil::toBool(cstr);
+	compactTransactions_ = DbUtil::toBool(cstr);
 }
 
 void Configuration::setCompareAsciiOnly(const char* cstr)
 {
-    compareAsciiOnly_ = DbUtil::toBool(cstr);
+	compareAsciiOnly_ = DbUtil::toBool(cstr);
 }
 
 void Configuration::setPrefixCurrency(bool val)
 {
-    prefixCurrency_ = val;
+	prefixCurrency_ = val;
 }
 
 void Configuration::setCompactTransactions(bool val)
 {
-    compactTransactions_ = val;
+	compactTransactions_ = val;
 }
 
 void Configuration::setCompareAsciiOnly(bool val)
 {
-    compareAsciiOnly_ = val;
+	compareAsciiOnly_ = val;
+}
+
+void Configuration::setHideZeroBalanceAccounts(bool val)
+{
+	hideZeroBalanceAccounts_ = val;
 }
 
 void Configuration::readConfiguration()
 {
-    ifstream fin(configurationFilePath_.c_str());
-    char buf[LINE_BUFFER_LENGTH];
-    fin.getline(buf, LINE_BUFFER_LENGTH);
-    lastDatabasePath_ = buf;
-    fin.close();
+	ifstream fin(configurationFilePath_.c_str());
+	char buf[LINE_BUFFER_LENGTH];
+	fin.getline(buf, LINE_BUFFER_LENGTH);
+	lastDatabasePath_ = buf;
+	fin.close();
 }
 
 void Configuration::writeConfiguration()
 {
-    ofstream fout(configurationFilePath_.c_str(), ofstream::out | ofstream::trunc);
-    fout << lastDatabasePath_ << endl;
-    fout.close();
+	ofstream fout(configurationFilePath_.c_str(), ofstream::out | ofstream::trunc);
+	fout << lastDatabasePath_ << endl;
+	fout.close();
 }
 
