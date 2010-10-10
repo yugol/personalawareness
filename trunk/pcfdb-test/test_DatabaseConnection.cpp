c1e3ab0dfb9ff91765bbd32caf7d40ad6ad0a368
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2010-10-10 08:39:43 +0000

    refactoring

--- trunk/pcfdb-test/test_DatabaseConnection.cpp
+++ trunk/pcfdb-test/test_DatabaseConnection.cpp
@@ -7,93 +7,93 @@
 
 TEST( GetBalance, DatabaseConnection )
 {
-    DatabaseConnection::openDatabase(testDatabase);
+	DatabaseConnection::openDatabase(testDatabase);
 
-    const Account* acc = DatabaseConnection::instance()->getAccount(1);
-    LONGS_EQUAL(Account::ACCOUNT, acc->getType());
+	const Account* acc = DatabaseConnection::instance()->getAccount(1);
+	LONGS_EQUAL(Account::ACCOUNT, acc->getType());
 
-    double balance = DatabaseConnection::instance()->getBalance(acc);
-    LONGS_EQUAL(90, balance);
+	double balance = DatabaseConnection::instance()->getBalance(acc);
+	LONGS_EQUAL(90, balance);
 }
 
 TEST( Accounts, DatabaseConnection )
 {
-    DatabaseConnection::openDatabase(testDatabase);
-    vector<int> sel;
+	DatabaseConnection::openDatabase(testDatabase);
+	vector<int> sel;
 
-    DatabaseConnection::instance()->getAccounts(&sel);
-    LONGS_EQUAL(1, sel.size());
+	DatabaseConnection::instance()->getAccounts(&sel);
+	LONGS_EQUAL(1, sel.size());
 
-    sel.clear();
-    DatabaseConnection::instance()->getCreditingBudgets(&sel);
-    LONGS_EQUAL(1, sel.size());
+	sel.clear();
+	DatabaseConnection::instance()->getCreditingBudgets(&sel);
+	LONGS_EQUAL(1, sel.size());
 
-    sel.clear();
-    DatabaseConnection::instance()->getDebitingBudgets(&sel);
-    LONGS_EQUAL(1, sel.size());
+	sel.clear();
+	DatabaseConnection::instance()->getDebitingBudgets(&sel);
+	LONGS_EQUAL(1, sel.size());
 
-    sel.clear();
-    DatabaseConnection::instance()->getBudgetCategories(&sel);
-    LONGS_EQUAL(2, sel.size());
+	sel.clear();
+	DatabaseConnection::instance()->getBudgetCategories(&sel);
+	LONGS_EQUAL(2, sel.size());
 }
 
 TEST( Transaction, DatabaseConnection )
 {
-    DatabaseConnection::openDatabase(testDatabase);
+	DatabaseConnection::openDatabase(testDatabase);
 
-    Transaction t(1);
-    DatabaseConnection::instance()->getTransaction(&t);
+	Transaction t(1);
+	DatabaseConnection::instance()->getTransaction(&t);
 
-    LONGS_EQUAL(1, t.getId());
-    LONGS_EQUAL(100, t.getValue());
-    LONGS_EQUAL(3, t.getFromId());
-    LONGS_EQUAL(1, t.getToId());
-    LONGS_EQUAL(1, t.getItemId());
+	LONGS_EQUAL(1, t.getId());
+	LONGS_EQUAL(100, t.getValue());
+	LONGS_EQUAL(3, t.getFromId());
+	LONGS_EQUAL(1, t.getToId());
+	LONGS_EQUAL(1, t.getItemId());
 
-    try {
-        Transaction nt(100);
-        DatabaseConnection::instance()->getTransaction(&nt);
-        FAIL(Exception::NO_RECORD_MESSAGE);
-    } catch (const Exception& ex) {
-    }
+	try {
+		Transaction nt(100);
+		DatabaseConnection::instance()->getTransaction(&nt);
+		FAIL(Exception::EMSG_NO_RECORD);
+	} catch (const Exception& ex) {
+	}
 }
 
 TEST( Items, DatabaseConnection )
 {
-    DatabaseConnection::openDatabase(testDatabase);
+	DatabaseConnection::openDatabase(testDatabase);
 
-    vector<int> sel;
-    DatabaseConnection::instance()->selectItems(&sel, 0);
-    LONGS_EQUAL(2, sel.size());
+	vector<int> sel;
+	DatabaseConnection::instance()->selectItems(&sel, 0);
+	LONGS_EQUAL(2, sel.size());
 
-    const Item* item = DatabaseConnection::instance()->getItem("out");
-    CHECK( 0 != item );
-    LONGS_EQUAL( 2, item->getId() );
-    LONGS_EQUAL( 2, item->getLastTransactionId() );
+	const Item* item = DatabaseConnection::instance()->getItem("out");
+	CHECK( 0 != item );
+	LONGS_EQUAL( 2, item->getId() );
+	LONGS_EQUAL( 2, item->getLastTransactionId() );
 }
 
 TEST( Sql, DatabaseConnection )
 {
-    DatabaseConnection::openDatabase(testDatabase);
+	DatabaseConnection::openDatabase(testDatabase);
 
-    char dumpFile[] = "dump.sql";
-    ::remove(dumpFile);
+	char dumpFile[] = "dump.sql";
+	::remove(dumpFile);
 
-    try {
+	try {
 
-        ofstream fout(dumpFile);
-        DatabaseConnection::exportDatabase(fout);
-        fout.close();
+		ofstream fout(dumpFile);
+		DatabaseConnection::exportDatabase(fout);
+		fout.close();
 
-        ifstream fin(dumpFile);
-        DatabaseConnection::openDatabase(disposableDatabase);
-        DatabaseConnection::importDatabase(fin);
-        fin.close();
-        DatabaseConnection::deleteDatabase();
+		ifstream fin(dumpFile);
+		DatabaseConnection::openDatabase(disposableDatabase);
+		DatabaseConnection::importDatabase(fin);
+		fin.close();
+		DatabaseConnection::deleteDatabase();
 
-        ::remove(dumpFile);
+		::remove(dumpFile);
 
-    } catch (const exception& ex) {
-        FAIL( ex.what() );
-    }
+	} catch (const exception& ex) {
+		FAIL( ex.what() );
+	}
 }
