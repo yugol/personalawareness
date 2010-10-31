b83dc7c6a9130758bd295d131cb441326bcfce63
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2010-10-31 14:56:23 +0000

    Moody console interface

---
+++ moody/src/moody.cpp
@@ -0,0 +1,32 @@
+#ifdef WINDOWS
+#include <direct.h>
+#define getcwd_api _getcwd
+#else
+#include <unistd.h>
+#define getcwd_api getcwd
+#endif
+
+#include <cerrno>
+#include <cstdio>
+#include <iostream>
+#include <Agent.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    char cwd[FILENAME_MAX];
+    if (!getcwd_api(cwd, sizeof(cwd))) {
+        cerr << "Cannot read working directory" << endl;
+        return errno;
+    }
+
+    cout << "Moody 0.0.1 (Meta Objects Organized DYnamically)" << endl;
+    cout << "Working directory: " << cwd << endl;
+
+    Agent agent(cin, cout, cerr);
+    agent.setInteractive();
+    agent.start();
+
+    return 0;
+}
