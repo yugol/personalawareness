b57a0cd7c407db4e7810956d3c66e0d1b5e41170
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2010-11-03 19:11:22 +0000

    database checking and optimization

--- trunk/pcfbase/inc/DatabaseConnection.h
+++ trunk/pcfbase/inc/DatabaseConnection.h
@@ -8,6 +8,7 @@
 #include "Account.h"
 #include "Item.h"
 #include "UndoBuffer.h"
+#include "OptimizationReport.h"
 
 class Transaction;
 class SelectionParameters;
@@ -16,95 +17,98 @@ class ReversibleDatabaseCommand;
 
 class DatabaseConnection {
 public:
-	enum ErrCodes {
-		OK = 0, STATEMENT_ERROR, EMPTY_DATABASE, WRONG_DATABASE
-	};
+    enum ErrCodes {
+        OK = 0, STATEMENT_ERROR, EMPTY_DATABASE, WRONG_DATABASE
+    };
 
-	static DatabaseConnection* instance();
-	static void openDatabase(const char* databasePath);
-	static void closeDatabase();
-	static void deleteDatabase();
-	static void exportDatabase(std::ostream& out);
-	static void importDatabase(std::istream& in);
-	static bool isOpened();
-	static bool isAccountInUse(sqlite3* database, int accountId);
-	static bool isItemInUse(sqlite3* database, int itemId);
+    static DatabaseConnection* instance();
+    static void openDatabase(const char* databasePath);
+    static void closeDatabase();
+    static void deleteDatabase();
+    static void exportDatabase(std::ostream& out);
+    static void importDatabase(std::istream& in);
+    static bool isOpened();
+    static bool isAccountInUse(sqlite3* database, int accountId);
+    static bool isItemInUse(sqlite3* database, int itemId);
 
-	~DatabaseConnection();
+    ~DatabaseConnection();
 
-	const std::string& getDatabaseLocation() const;
+    const std::string& getDatabaseLocation() const;
 
-	void insertUpdate(Account* account);
-	void deleteAccount(int id);
-	void selectAccounts(std::vector<int>* selection, SelectionParameters* parameters) const;
-	bool isAccountInUse(int id) const;
-	int getAccountCount() const;
-	void getAccount(Account* account) const;
-	const Account* getAccount(int id) const;
-	const Account* getAccount(const char* name) const;
-	double getBalance(const Account* account) const;
-	void getAccounts(std::vector<int>* sel) const;
-	void getBudgetCategories(std::vector<int>* sel) const;
-	void getCreditingBudgets(std::vector<int>* sel) const;
-	void getDebitingBudgets(std::vector<int>* sel) const;
+    void insertUpdate(Account* account);
+    void deleteAccount(int id);
+    void selectAccounts(std::vector<int>* selection, SelectionParameters* parameters) const;
+    bool isAccountInUse(int id) const;
+    int getAccountCount() const;
+    void getAccount(Account* account) const;
+    const Account* getAccount(int id) const;
+    const Account* getAccount(const char* name) const;
+    double getBalance(const Account* account) const;
+    void getAccounts(std::vector<int>* sel) const;
+    void getBudgetCategories(std::vector<int>* sel) const;
+    void getCreditingBudgets(std::vector<int>* sel) const;
+    void getDebitingBudgets(std::vector<int>* sel) const;
 
-	void insertUpdate(Item* item);
-	void deleteItem(int id);
-	void selectItems(std::vector<int>* selection, SelectionParameters* parameters) const;
-	bool isItemInUse(int id) const;
-	void getItem(Item* item) const;
-	int getItemCount() const;
-	const Item* getItem(int id) const;
-	const Item* getItem(const char* name) const;
+    void insertUpdate(Item* item);
+    void deleteItem(int id);
+    void selectItems(std::vector<int>* selection, SelectionParameters* parameters) const;
+    bool isItemInUse(int id) const;
+    void getItem(Item* item) const;
+    int getItemCount() const;
+    const Item* getItem(int id) const;
+    const Item* getItem(const char* name) const;
 
-	void insertUpdate(Transaction* transaction);
-	void deleteTransaction(int id);
-	void selectTransactions(std::vector<int>* selection, SelectionParameters* parameters) const;
-	void getTransaction(Transaction* transaction) const;
-	void getFirstTransaction(Transaction* transaction) const;
-	void getLastTransaction(Transaction* transaction) const;
+    void insertUpdate(Transaction* transaction);
+    void deleteTransaction(int id);
+    void selectTransactions(std::vector<int>* selection, SelectionParameters* parameters) const;
+    void getTransaction(Transaction* transaction) const;
+    void getFirstTransaction(Transaction* transaction) const;
+    void getLastTransaction(Transaction* transaction) const;
 
-	const ReversibleDatabaseCommand* getUndo();
-	const ReversibleDatabaseCommand* getRedo();
-	void undo();
-	void redo();
+    const ReversibleDatabaseCommand* getUndo();
+    const ReversibleDatabaseCommand* getRedo();
+    void undo();
+    void redo();
+
+    void check(OptimizationReport* report);
+    void optimize(const OptimizationReport* report);
 
 private:
-	static void readPreferences(SelectPreferences& prefs);
-	static void writePreferences(sqlite3* database);
+    static void readPreferences(SelectPreferences& prefs);
+    static void writePreferences(sqlite3* database);
 
-	static DatabaseConnection* instance_;
+    static DatabaseConnection* instance_;
 
-	std::string databaseLocation_;
-	sqlite3* database_;
-	UndoBuffer undoBuffer_;
-	mutable std::vector<Account> accounts_;
-	mutable std::map<int, Item> items_;
+    std::string databaseLocation_;
+    sqlite3* database_;
+    UndoBuffer undoBuffer_;
+    mutable std::vector<Account> accounts_;
+    mutable std::map<int, Item> items_;
 
-	DatabaseConnection(const char* location);
-	DatabaseConnection(const DatabaseConnection&);
-	void operator=(const DatabaseConnection&);
+    DatabaseConnection(const char* location);
+    DatabaseConnection(const DatabaseConnection&);
+    void operator=(const DatabaseConnection&);
 
-	void createNewDatabase();
-	void openConnection();
-	int getTableCount();
-	void purgeDatabase();
-	void closeConnection(bool purge);
+    void createNewDatabase();
+    void openConnection();
+    int getTableCount();
+    void purgeDatabase();
+    void closeConnection(bool purge);
 
-	void cashAccounts() const;
-	void cashItems() const;
+    void cashAccounts() const;
+    void cashItems() const;
 
-	void invalidateAccounts() const;
-	void invalidateItems() const;
-	void invalidateCash() const;
+    void invalidateAccounts() const;
+    void invalidateItems() const;
+    void invalidateCash() const;
 
-	void dumpSql(std::ostream& out) const;
-	void loadSql(std::istream& in);
+    void dumpSql(std::ostream& out) const;
+    void loadSql(std::istream& in);
 };
 
 inline const std::string& DatabaseConnection::getDatabaseLocation() const
 {
-	return databaseLocation_;
+    return databaseLocation_;
 }
 
 #endif // DATABASECONNECTION_H
