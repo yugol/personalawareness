495141f7bf7adf0a0f683e138ad04ac837cf2565
iulian.goriac <iulian.goriac@be78379c-ac1e-11dd-a5b1-1f8032f4537c>
2010-11-07 20:50:52 +0000

    refactoring

--- trunk/pacommon/src/ConfigurationBase.cpp
+++ trunk/pacommon/src/ConfigurationBase.cpp
@@ -11,12 +11,12 @@ const char ConfigurationBase::CONFIGURATION_FILEEXT[] = ".awareness.cfg";
 
 ConfigurationBase::ConfigurationBase()
 {
-	if (supportsConfigurationFile()) {
-		ostringstream sout;
-		sout << ::getenv(HOME_ENVIRONMENT_VARIABLE_NAME) << "/" << CONFIGURATION_FILEEXT;
-		configurationFilePath_ = sout.rdbuf()->str();
-		readConfigurationFile();
-	}
+    if (supportsConfigurationFile()) {
+        ostringstream sout;
+        sout << ::getenv(HOME_ENVIRONMENT_VARIABLE_NAME) << "/" << CONFIGURATION_FILEEXT;
+        configurationFileLocation_ = sout.rdbuf()->str();
+        readConfigurationFile();
+    }
 }
 
 ConfigurationBase::~ConfigurationBase()
@@ -25,42 +25,42 @@ ConfigurationBase::~ConfigurationBase()
 
 bool ConfigurationBase::supportsConfigurationFile() const
 {
-	return (::getenv(HOME_ENVIRONMENT_VARIABLE_NAME) != NULL);
+    return (::getenv(HOME_ENVIRONMENT_VARIABLE_NAME) != NULL);
 }
 
 bool ConfigurationBase::existsConfigurationFile() const
 {
-	ifstream fin(configurationFilePath_.c_str());
-	return fin.is_open();
+    ifstream fin(configurationFileLocation_.c_str());
+    return fin.is_open();
 }
 
 void ConfigurationBase::createConfigurationFile()
 {
-	ofstream fout(configurationFilePath_.c_str(), ofstream::out | ofstream::trunc);
-	fout.close();
+    ofstream fout(configurationFileLocation_.c_str(), ofstream::out | ofstream::trunc);
+    fout.close();
 }
 
 void ConfigurationBase::deleteConfigurationFile()
 {
-	::remove(configurationFilePath_.c_str());
+    ::remove(configurationFileLocation_.c_str());
 }
 
 void ConfigurationBase::readConfigurationFile()
 {
-	if (existsConfigurationFile()) {
-		ifstream fin(configurationFilePath_.c_str());
-		char buf[LINE_BUFFER_LENGTH];
-		fin.getline(buf, LINE_BUFFER_LENGTH);
-		lastDatabasePath_ = buf;
-		fin.close();
-	}
+    if (existsConfigurationFile()) {
+        ifstream fin(configurationFileLocation_.c_str());
+        char buf[LINE_BUFFER_LENGTH];
+        fin.getline(buf, LINE_BUFFER_LENGTH);
+        lastDatabaseLocation_ = buf;
+        fin.close();
+    }
 }
 
 void ConfigurationBase::writeConfigurationFile()
 {
-	if (existsConfigurationFile()) {
-		ofstream fout(configurationFilePath_.c_str(), ofstream::out | ofstream::trunc);
-		fout << lastDatabasePath_ << endl;
-		fout.close();
-	}
+    if (existsConfigurationFile()) {
+        ofstream fout(configurationFileLocation_.c_str(), ofstream::out | ofstream::trunc);
+        fout << lastDatabaseLocation_ << endl;
+        fout.close();
+    }
 }
